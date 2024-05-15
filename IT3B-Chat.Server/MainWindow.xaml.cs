17589a920f942133a167095edd1d2830394a40b7
Ondřej Kryštof <Krystof.Ondrej@stud.vassboskovice.cz>
2024-05-15 13:12:44 +0200

    Neco jsem udelal, ale karel to neschvalil, takze budu pokracovat

--- IT3B-Chat.Server/MainWindow.xaml.cs
+++ IT3B-Chat.Server/MainWindow.xaml.cs
@@ -11,14 +11,19 @@ using System.Windows.Shapes;
 
 namespace IT3B_Chat.Server
 {
- /// <summary>
- /// Interaction logic for MainWindow.xaml
- /// </summary>
- public partial class MainWindow : Window
- {
-  public MainWindow()
-  {
-   InitializeComponent();
-  }
- }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Přidat ukázková data
+            MessagesListBox.Items.Add("Zpráva 1 od klienta A");
+            MessagesListBox.Items.Add("Zpráva 2 od klienta B");
+            MessagesListBox.Items.Add("Zpráva 3 od klienta C");
+
+            ConnectionsListBox.Items.Add("Klient A připojen");
+            ConnectionsListBox.Items.Add("Klient B odpojen");
+            ConnectionsListBox.Items.Add("Klient C připojen");
+        }
+    }
 }
